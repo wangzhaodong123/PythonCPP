8e0b73e72718c5909c060fd912a3165cd43ceab2
王召东 <342792314@qq.com>
2018-10-24 10:54:23 +0800

    Add files via upload

---
+++ libpython/libpython.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+def hello():
+  print('hello world!')
+def add_sub(x,y):
+  return x+y,x-y
+class Player:
+  def slamdunk(self):
+    print('I am slamdunk!')
+  def yell(self,t):
+    print(t)
